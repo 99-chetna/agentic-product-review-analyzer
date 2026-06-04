8750bcef44ff33a67bc9bc30849155b971aa1f89
Chetna Dadwal <chetnadadwal23@gmail.com>
2026-06-04 15:11:14 +0530

    Fixed Render port configuration

--- app.py
+++ app.py
@@ -268,15 +268,8 @@ def index():
     )
 
 
-import webbrowser
+import os
 
 if __name__ == "__main__":
-
-    webbrowser.open(
-        "http://127.0.0.1:5000"
-    )
-
-    app.run(
-        debug=True,
-        use_reloader=False
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
